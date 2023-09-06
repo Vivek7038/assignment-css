e840b03ac2b681b42d50ab90f2c5daf8495cac16
Vivek7038 <vivek888chavan@gmail.com>
2023-09-06 23:03:37 +0530

    2 sections done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,25 @@ import uparrow from "../assets/arrow-up.png";
 const Navbar = () => {
   return <>
   <div className="nav">
-    
+    <div className="nav-items">
+      <div className="nav-one">
+      <div className="selected">Home</div>
+      <div>About Us</div>
+      <div>Get Involved</div>
+      </div>
+      <div className="nav-two">
+      <div>logo</div>
+      <div>charify</div>
+     
+      </div>
+      <div className="nav-three">
+      <div>Donor</div>
+      <div>Stories</div>
+      <div>News & Events</div>
+     
+      </div>
+    </div>
+    <div className="horizontal-line"/>
   </div>
   </>;
 };
